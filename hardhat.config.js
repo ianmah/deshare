c94a87a36ccb7fef639bd8c102daec8587f2e3aa
unknown <ianmmah@gmail.com>
2022-02-04 15:46:58 -0800

    Update config and first deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
-};
+   solidity: "0.8.9",
+   defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
