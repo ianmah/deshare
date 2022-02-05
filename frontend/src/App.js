354915dcd041e1eb02d773d4a5bc1ec6f159d913
unknown <ianmmah@gmail.com>
2022-02-05 14:23:10 -0800

    Mint member token button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import { ethers } from 'ethers'
 
 
 import DeSharePost from './artifacts/contracts/DeShare.sol/DeSharePost.json'
+import DeShareMember from './artifacts/contracts/DeShare.sol/DeShareMember.json'
 
 const dspAddress = "0x72611d0fc2062C0115156a2f240eDbDbd9A1F53b"
+const dsmAddress = "0xBE806Cac1D25803fc97De268341040271CBf622c"
 
 // Specify your own API keys
 // Each is optional, and if you omit it the default
@@ -13,20 +15,24 @@ const provider = new ethers.providers.Web3Provider(window.ethereum)
 
 function App() {
   const [wallet, setWallet] = useState({})
-  const [contract, setContract] = useState()
+  const [contractP, setContractP] = useState()
+  const [contractM, setContractM] = useState()
 
   const mintMember = async () => {
-    const res = await contract.setMemberContract('0xbe806cac1d25803fc97de268341040271cbf622c')
-    console.log(res)
+    try {
+      await contractM.mintItem()
+    } catch (err) {
+      console.log(err.data.message)
+    }
   }
 
   useEffect(() => {
     async function connectEth() {
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner()
-      const address = await signer.getAddress()
-
-      setContract(new ethers.Contract(dspAddress, DeSharePost.abi, signer))
+      const address = await signer.getAddress()      
+      setContractP(new ethers.Contract(dspAddress, DeSharePost.abi, signer))
+      setContractM(new ethers.Contract(dsmAddress, DeShareMember.abi, signer))
 
       provider.getBalance(address).then((balance) => {
         // convert a currency unit from wei to ether
